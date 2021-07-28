60638cfc049a07f13800021b39ddf34406f65476
Zyao89 <zyao89@gmail.com>
2021-07-28 13:58:29 +0800

    feat: add env.port & env.host

--- src/plugins/factory/index.js
+++ src/plugins/factory/index.js
@@ -17,19 +17,22 @@ module.exports = async function(api, info = {}) {
         }
     });
 
+    // 上下文参数
+    const apiContext = api.context || {};
+
     const { index, port, host } = info;
     const runApp = require(index); // app.js
     await runApp(app);
 
     const portfinder = require('portfinder');
 
-    const startPort = parseInt(process.env.PORT || port || 3000);
+    const startPort = parseInt(process.env.PORT || apiContext.port || port || 3000);
     const _port = await portfinder.getPortPromise({
         port: startPort, // minimum port
         stopPort: startPort + 300, // maximum port
     });
 
-    const _host = process.env.HOST || host || '0.0.0.0';
+    const _host = process.env.HOST || apiContext.host || host || '0.0.0.0';
 
     return new Promise((resolve, reject) => {
         app.listen(_port, _host, err => {
