731fdb0064a53cf6c507154e0d885fda872b2fae
Zyao89 <zyao89@gmail.com>
2022-05-20 11:11:21 +0800

    feat: 优化日志打印判断逻辑

--- src/plugins/factory/_koaLogger.js
+++ src/plugins/factory/_koaLogger.js
@@ -9,39 +9,51 @@ module.exports = function(app) {
     const NAME = config.name || config.info.name || CONSTANTS.NAME;
 
     // 日志等级
-    const LOGGER_LEVEL = process.env.MICROAPP_LOGGER_LEVEL || 5;
+    const LEVEL_MAP = {
+        debug: 1,
+        error: 2,
+        warn: 3,
+        info: 4,
+        success: 4,
+        printf: 4,
+    };
+    let LOGGER_LEVEL = 5;
+    if (process.env.MICROAPP_LOGGER_LEVEL && typeof process.env.MICROAPP_LOGGER_LEVEL === 'string') {
+        LOGGER_LEVEL = LEVEL_MAP[process.env.MICROAPP_LOGGER_LEVEL.toLowerCase()];
+        LOGGER_LEVEL = Number(LOGGER_LEVEL || process.env.MICROAPP_LOGGER_LEVEL);
+    }
 
     const customLogger = {
         debug(...args) {
-            if (Number(LOGGER_LEVEL) <= 0) {
+            if (LOGGER_LEVEL <= LEVEL_MAP.debug) {
                 return;
             }
             const time = getTime();
             logger.debug(`[${time}]`, ...args);
         },
         error(...args) {
-            if (Number(LOGGER_LEVEL) <= 1) {
+            if (LOGGER_LEVEL <= LEVEL_MAP.error) {
                 return;
             }
             const time = getTime();
             logger.error(`[${time}]`, ...args);
         },
         warn(...args) {
-            if (Number(LOGGER_LEVEL) <= 2) {
+            if (LOGGER_LEVEL <= LEVEL_MAP.warn) {
                 return;
             }
             const time = getTime();
             logger.warn(`[${time}]`, ...args);
         },
         info(...args) {
-            if (Number(LOGGER_LEVEL) <= 4) {
+            if (LOGGER_LEVEL <= LEVEL_MAP.info) {
                 return;
             }
             const time = getTime();
             logger.info(`[${time}]`, ...args);
         },
         success(...args) {
-            if (Number(LOGGER_LEVEL) <= 4) {
+            if (LOGGER_LEVEL <= LEVEL_MAP.success) {
                 return;
             }
             const time = getTime();
@@ -49,7 +61,7 @@ module.exports = function(app) {
         },
 
         printf(name = '', ...args) {
-            if (Number(LOGGER_LEVEL) <= 4) {
+            if (LOGGER_LEVEL <= LEVEL_MAP.printf) {
                 return;
             }
             const time = getTime();
